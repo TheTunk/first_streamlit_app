1a68bead0154c647fa9930b53f12f6bd2e46a7af
TheTunk <73656647+TheTunk@users.noreply.github.com>
2022-10-02 00:54:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,4 +77,4 @@ streamlit.write('Thanks for adding jackfruit', add_my_fruit)
 
 
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
